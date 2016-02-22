cc411f6ad744157282bf312830702ccf8ed49e94
Juan Carlos Juárez <isc.jcjl@me.com>
2016-02-21 23:12:59 -0600

    Iniciando repositorio

---
+++ app/src/main/java/com/jcarlos/shunshine/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jcarlos.shunshine;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new BlankFragment()).commit();
+        }
+    }
+}
